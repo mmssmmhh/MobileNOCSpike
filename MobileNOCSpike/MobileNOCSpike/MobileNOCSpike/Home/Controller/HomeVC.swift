72b94ec5c2a3db089bedb73ed5f0582ba4e710a0
Mohamed Kelany <mohamed.kelany@outlook.com>
2019-03-01 18:44:39 +0200

    creating Home files

--- MobileNOCSpike/MobileNOCSpike/MobileNOCSpike/Home/Controller/HomeVC.swift
+++ MobileNOCSpike/MobileNOCSpike/MobileNOCSpike/Home/Controller/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  MobileNOCSpike
 //
 //  Created by Mohamed Kelany on 3/1/19.
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
---
+++ MobileNOCSpike/MobileNOCSpike/MobileNOCSpike/Home/Serivces/HomeServices.swift
@@ -0,0 +1,9 @@
+//
+//  HomeServices.swift
+//  MobileNOCSpike
+//
+//  Created by Mohamed Kelany on 3/1/19.
+//  Copyright © 2019 Mohamed Kelany. All rights reserved.
+//
+
+import Foundation
