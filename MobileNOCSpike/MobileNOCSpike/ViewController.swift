a0621aba9710bcdf43f21032d8ff3ba004675b5e
Mohamed Kelany <mohamed.kelany@outlook.com>
2019-03-01 17:22:28 +0200

    Creating inital Project with needed Pods

---
+++ MobileNOCSpike/MobileNOCSpike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileNOCSpike
+//
+//  Created by Mohamed Kelany on 3/1/19.
+//  Copyright © 2019 Mohamed Kelany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MobileNOCSpike/Pods/Target Support Files/Pods-MobileNOCSpikeUITests/Pods-MobileNOCSpikeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileNOCSpikeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MobileNOCSpikeUITests
+@end
